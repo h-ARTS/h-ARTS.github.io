f1cfd1efa565819bd2782161d5d69fa48aaea778
Hanan Mufti <hi5hanan@yahoo.com>
2017-06-23 11:51:45 +0200

    added credentials

--- react/to-let/js/app.jsx
+++ react/to-let/js/app.jsx
@@ -1,4 +1,4 @@
-var IntroFormHandler = React.createClass({
+﻿var IntroFormHandler = React.createClass({
      render: function() {
           return(
                <div className="jumbotron jumbotron-fluid">
@@ -161,7 +161,9 @@ var App = React.createClass({
                input: input
           });
           var _this = this;
-          axios.get("https://api.nestoria.co.uk/api?encoding=json&pretty=1&action=search_listings&country=uk&listing_type=rent&place_name=" + input)
+          axios.get("https://api.nestoria.co.uk/api?encoding=json&pretty=1&action=search_listings&country=uk&listing_type=rent&place_name=" + input, {
+                withCredentials: true
+              })
                .then(function(res) {
                     _this.setState({
                          flats: res.data.response.listings
